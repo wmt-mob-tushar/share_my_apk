06f76a89adc1ca8a8537424907c60b871ddb4eaa
Jenil D Gohel <jenilg@webmobtech.com>
2025-07-08 17:50:42 +0530

    chore: Remove tmp directory and update pubspec
    The tmp directory is no longer needed and has been removed from the repository. The pubspec.yaml has been updated to remove unnecessary fields.

--- tmp/lib/src/services/upload/upload_service.dart
+++
@@ -1,8 +0,0 @@
-/// An abstract class that defines the interface for an upload service.
-abstract class UploadService {
-  /// Uploads a file to a remote service.
-  ///
-  /// Returns a [Future] that completes with the download URL of the uploaded
-  /// file.
-  Future<String> upload(String filePath);
-}
